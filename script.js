121d5a886068157d0516103231d135a3cf0e5565
prtus007 <sumanpratyush90@gmail.com>
2025-06-06 10:18:44 +0530

    Updating To-Do List project files

--- script.js
+++ script.js
@@ -15,13 +15,39 @@ addEventListener("keydown", function removing(event) {
     }
 })
 function adder(text) {
+    if (text.length!=0) {
         const para = document.createElement("li")
         para.innerText = text;
+        para.style.display = "flex"
+        para.style.justifyContent = "space-between";
+        para.style.alignItems = "center";
         document.querySelector(".obj").appendChild(para);
+        para.textContent=document.getElementById('Enter').value;
+        document.getElementById('Enter').value="";
+        deleting(para);
+    }
 }
 function removing(task) {
     const str = document.querySelector(".obj");
     str.removeChild(str.firstElementChild);
 }
-
+function deleting(para) {
+      const delbtn = document.createElement('button');
+      delbtn.textContent = "REM";
+      delbtn.style.height="20px"
+      delbtn.style.width="40px"
+      delbtn.style.border="0px"
+      delbtn.style.borderRadius="5px"
+      delbtn.style.backgroundColor="yellow"
+      para.appendChild(delbtn);
+      delbtn.addEventListener("click",function() {
+          para.remove();
+      });
+      delbtn.addEventListener("mouseover",function () {
+        delbtn.style.backgroundColor="#d86c6c"
+      })
+      delbtn.addEventListener("mouseout",function () {
+        delbtn.style.backgroundColor="yellow";
+      })
+}
 
